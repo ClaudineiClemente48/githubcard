4cf7e95622d87e8c9448d7427447d1446e6d9392
ClaudineiClemente48 <146371914+ClaudineiClemente48@users.noreply.github.com>
2025-02-13 15:25:39 -0300

    cartão fim

--- app.js
+++ app.js
@@ -6,10 +6,29 @@ fetch(url)
     return response.json()
   })
   .then(dados => {
-    console.dir(dados)
+    
+    fnCarregarDados(dados.avatar_url, dados.name, dados.bio, dados.email, dados.twitter_username, dados.html_url, dados.blog)
   })
 
 
 }
+
+function fnMostrarDados(fotos,nome, resumo,email,twitter,github, blog, ){
+  document.getElementById("foto").src = fotos
+  document.getElementById("nome").innerHTML = nome 
+  document.getElementById("resumo").innerHTML = resumo + "<br> <strong class='email' id='email'></strong>"
+  document.getElementById("email").innerHTML = email
+  document.getElementById("twitter").href = twitter
+  document.getElementById("github").href = github
+  document.getElementById("blog").href = blog
+}
+let botao = document.getElementById("botao-carregar-dados")
+botao.addEventListener("click", function(){
+
+  fnCarregarDados()
+
+})
+
+
 
 
